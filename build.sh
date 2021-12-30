3d57a8032950fe7f7ab27890da564a03792423d9
Mariusz Kryński <mrk@sed.pl>
2021-12-30 16:03:00 +0100

    add build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+./x.py build -i --stage 0 src/tools/cargo
+./x.py build -i
+ln -s ../../stage0-tools-bin/cargo build/x86_64-unknown-linux-gnu/stage1/bin/cargo
+rustup toolchain link mos build/x86_64-unknown-linux-gnu/stage1
