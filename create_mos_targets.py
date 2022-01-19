dfaf693fdc8e3857a5e1e906165ab577a4470133
Mariusz Kryński <mrk@sed.pl>
2022-01-19 21:39:25 +0100

    fix create_mos_targets script

--- create_mos_targets.py
+++ create_mos_targets.py
@@ -9,17 +9,10 @@ CFG_FILES = {
     "a800xl": "atari/800xl.cfg",
 }
 
-def create_target(target_spec, sdk_build_dir, platform):
+def create_target(target_spec, platform):
     opts = target_spec.copy()
     opts.pop("is-builtin")    
     opts["linker"] = f"mos-{platform}-clang"
-    # config_path = CFG_FILES[platform]
-    # opts["pre-link-args"] = {
-    #     "gcc": [
-    #         "--config",
-    #         os.path.join(sdk_build_dir, config_path),
-    #     ]
-    # }
     opts["vendor"] = platform
     return opts
 
@@ -29,10 +22,11 @@ if __name__ == "__main__":
         subprocess.getoutput('rustup run mos rustc --target mos-unknown-none -Z unstable-options --print target-spec-json')
     )
 
-    sdk_build_dir = sys.argv[1]
-    dest_dir = sys.argv[2]
+    dest_dir = sys.argv[1]
 
     for arch in ("a800xl", "c64", "sim"):
-        target_def = create_target(target_spec, sdk_build_dir, arch)
-        with open(os.path.join(dest_dir, f"mos-{arch}-none.json"), "w") as fp:
+        target_def = create_target(target_spec, arch)
+        target_file = os.path.join(dest_dir, f"mos-{arch}-none.json")
+        with open(target_file, "w") as fp:
             json.dump(target_def, fp, indent=2)
+            print("created", target_file)
