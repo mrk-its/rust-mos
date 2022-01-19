744105428ea766266435b775fadf8e32c6179b0f
Mariusz Kryński <mrk@sed.pl>
2022-01-19 21:39:25 +0100

    fix create_mos_targets script

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 ln -s ../../stage0-tools-bin/cargo build/x86_64-unknown-linux-gnu/stage1/bin/cargo
 rustup toolchain link mos build/x86_64-unknown-linux-gnu/stage1
 rustup default mos
+mkdir -p $RUST_TARGET_PATH
+python3 create_mos_targets.py $RUST_TARGET_PATH
